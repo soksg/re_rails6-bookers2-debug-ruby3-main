06f68ce26d99a270e25531800843fc5a98851831
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-10-06 08:21:56 +0000

    [Add]応用課題２　users/index修正開始

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+    @new_book = Book.new
   end
 
   def index
     @users = User.all
-    @book = Book.new
+    @new_book = Book.new
+    @book=Book.new
+    @user=current_user
   end
 
   def edit
