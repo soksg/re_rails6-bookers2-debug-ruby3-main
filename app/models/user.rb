b6c1fcb728a37ca5b56a53c10f84c5e1bb92e1e6
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-10-10 07:10:10 +0000

    [Add]応用課題復習　検索機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,5 +47,18 @@ class User < ApplicationRecord
   end
 
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user=User.where("name LIKE?", "#{word}")
+    elsif search == "foward_match"
+      @user=User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user=User.where("name LIKE?", "%#{word}")
+    elsif search == "pertial_match"
+      @user=User.where("name LIKE?", "%#{word}%")
+    else
+      @user=User.all
+    end
+  end
 
 end
